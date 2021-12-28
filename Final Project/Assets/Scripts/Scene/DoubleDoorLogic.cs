ef7243201a741669e4edfb38d1ceb2d347f43b02
unknown <>
2021-12-28 16:08:39 +0800

    pull

--- Final Project/Assets/Scripts/Scene/DoubleDoorLogic.cs
+++ Final Project/Assets/Scripts/Scene/DoubleDoorLogic.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class DoubleDoorLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void EquipWeapon(GameObject interactionObject)
+    {
+        // Disable Gravity on RigidBody
+        Rigidbody rigidBody = interactionObject.GetComponent<Rigidbody>();
+        if (rigidBody)
+        {
+            rigidBody.useGravity = false;
+        }
+
+        // Disable first BoxCollider
+        BoxCollider boxCollider = interactionObject.GetComponent<BoxCollider>();
+        if (boxCollider)
+        {
+            boxCollider.enabled = false;
+        }
+
+        // Set Position and Direction of Gun
+        interactionObject.transform.position = m_GunAttachmentTransform.position;
+        interactionObject.transform.forward = m_GunAttachmentTransform.forward;
+
+        // Parent Gun to Attach Point
+        interactionObject.transform.parent = m_GunAttachmentTransform;
+
+        GunLogic gunLogic = interactionObject.GetComponent<GunLogic>();
+        if (gunLogic)
+        {
+            gunLogic.SetEquipped(true);
+        }
+
+        m_equippedObject = m_interactionObject;
     }
 }
