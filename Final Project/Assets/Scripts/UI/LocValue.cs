b29e9400141fd3c6fa61ca98fac0f4f21790b239
NaiL23 <lianyh@shanghaitech.edu.cn>
2022-01-01 02:21:08 +0800

    add LocalizationManager

---
+++ Final Project/Assets/Scripts/UI/LocValue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocValue : MonoBehaviour
+{
+    [SerializeField]
+    string m_key;
+
+    void Start()
+    {
+        LoadLocalizationData();
+    }
+
+    public void LoadLocalizationData() {
+        Text text = GetComponent<Text>();
+        if(text && m_key != "")
+        {
+            text.text = LocalizationManager.Instance.GetLocalizedValue(m_key);
+        }
+    }
+}
